79be9fe05a8e30a9e9cf6310b5575911df290d79
serkanaydin <aWsa%4582>
2021-06-15 14:39:33 +0300

    inverse kinematics articulate

--- articulateInverseKinematics.py
+++ articulateInverseKinematics.py
@@ -1,5 +1,4 @@
 import math
-import time
 
 import numpy
 from vpython import *
@@ -17,29 +16,42 @@ cyl2 = cylinder(
     radius=1,
     color=vector(0.5, 0, 0))
 
-cyl3= cylinder(
-    axis=vector(0,1, 0),
+cyl3 = cylinder(
+    axis=vector(0, 1, 0),
     length=4,
     radius=1,
     color=vector(0.5, 0, 0))
 
-theta0 = math.radians(0)
-theta1 = math.radians(45)
-theta2 = math.radians(45)
-print(sin(theta1))
+endX   =10
+endY = 10
+endZ = 10
+
 a1 = 8
 a2 = 8
 a3 = 8
-a4 = 8
 
-d = 8
+r1 = math.sqrt(math.pow(endX, 2) + math.pow(endY, 2))
+r2=endZ-a1
+r3= math.sqrt(math.pow(r1,2)+math.pow(r2,2))
+fi2 = math.atan(r2/r1)
+cosfi1=(math.pow(a3,2)-math.pow(a2,2) -math.pow(r3,2))
+fi1=math.acos((math.pow(a3,2)-math.pow(a2,2) -math.pow(r3,2))/(-2*a2*r3))
+fi3=math.acos((math.pow(r3,2)-math.pow(a3,2)-math.pow(a2,2))/(-2*a2*a3))
+
+theta0=math.atan(endY/endX)
+theta1=fi2-fi1
+theta2=math.pi-fi3
+
+
+
+
 R01 = np.matmul([[cos(theta0), -sin(theta0), 0],
                  [sin(theta0), cos(theta0), 0],
                  [0, 0, 1]],
                 [[1, 0, 0],
                  [0, 0,-1],
                  [0, 1, 0]])
-d01 = [[a2 * cos(theta0)], [a2 * sin(theta0)], [a1]]
+d01 = [[0], [0], [a1]]
 
 H01 = [numpy.append(R01[0], d01[0][0]),
        numpy.append(R01[1], d01[1][0]),
@@ -52,7 +64,7 @@ R12 = np.matmul([[cos(theta1), -sin(theta1), 0],
                 [[1, 0, 0],
                  [0, 1, 0],
                  [0, 0, 1]])
-d12 = [[a3*cos(theta1)], [a3*sin(theta1)], [0]]
+d12 = [[a2*cos(theta1)], [a2*sin(theta1)], [0]]
 
 H12 = [numpy.append(R12[0], d12[0][0]),
        numpy.append(R12[1], d12[1][0]),
@@ -64,19 +76,20 @@ R23 = np.matmul([[cos(theta2), -sin(theta2), 0],
                  [0, 0, 1]],
                 [[1, 0, 0], [0, 1, 0], [0, 0, 1]])
 
-d23 = [[a4*cos(theta2)], [a4*sin(theta2)],[0]]
+d23 = [[a3*cos(theta2)], [a3*sin(theta2)],[0]]
 
 H23 = [numpy.append(R23[0], d23[0][0]),
        numpy.append(R23[1], d23[1][0]),
        numpy.append(R23[2], d23[2][0]),
        [0, 0, 0, 1]]
-
+3
 H02 = np.matmul(H01, H12)
 H03 = np.matmul(H02, H23)
 cyl2.pos = vector(H01[0][3], H01[1][3], H01[2][3])
 cyl3.pos = vector(H02[0][3], H02[1][3], H02[2][3])
 
-a1c = curve(vector(0, 0, 0), vector(0, 0, a3))
-a2c = curve(vector(0, 0, a3), cyl2.pos)
-a3c = curve(cyl2.pos, cyl3.pos)
-a4c = curve(cyl3.pos, vector(H03[0][3], H03[1][3], H03[2][3]))
+a1c = curve(vector(0, 0, 0), vector(0, 0, a1))
+a2c = curve(vector(0, 0, a1), cyl3.pos)
+a3c = curve(cyl3.pos, vector(H03[0][3], H03[1][3], H03[2][3]))
+
+print(vector(H03[0][3], H03[1][3], H03[2][3]))
